f3862a1db02e48135141a4b323fe1b1ad46f16dc
haibo.li <531003231@qq.com>
2022-02-10 15:28:33 +0800

    [add] 初始化项目,测试索引新增和删除

---
+++ springdata-es/src/main/java/com/lhb/es/DAO/ProductDao.java
@@ -0,0 +1,9 @@
+package com.lhb.es.DAO;
+
+import com.lhb.es.entity.Product;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductDao extends ElasticsearchRepository<Product,Long> {
+}
---
+++ springdata-es/src/main/java/com/lhb/es/config/ElasticsearchConfig.java
@@ -0,0 +1,34 @@
+package com.lhb.es.config;
+
+import lombok.Data;
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
+
+@ConfigurationProperties(prefix = "elasticsearch")
+@Configuration
+@Data
+public class ElasticsearchConfig extends AbstractElasticsearchConfiguration {
+
+    private String host ;
+    private Integer port ;
+
+    /**
+     * 重写方法
+     * @Author haibo.li
+     * @Date 2022/2/10 14:57
+     * @Param []
+     * @Return RestHighLevelClient
+     */
+    @Override
+    public RestHighLevelClient elasticsearchClient() {
+        RestClientBuilder builder = RestClient.builder(new HttpHost(host, port));
+        RestHighLevelClient  restHighLevelClient  =  new
+                RestHighLevelClient(builder);
+        return restHighLevelClient;
+    }
+}
