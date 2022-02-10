1da763ad9819401adf124344fdea28a1edb33871
haibo.li <531003231@qq.com>
2022-02-10 16:14:11 +0800

    [fix] fix

--- springdata-es/src/main/java/com/lhb/es/estest/SpringDataESProductDaoTest.java
+++ springdata-es/src/main/java/com/lhb/es/estest/SpringDataESProductDaoTest.java
@@ -105,10 +105,10 @@ public class SpringDataESProductDaoTest {
     public void findByPageable(){
         //设置排序(排序方式，正序还是倒序，排序的 id)
         Sort sort = Sort.by(Sort.Direction.DESC,"id");
-        int currentPage=1;//当前页，第一页从 0 开始，1 表示第二页
+        int pageNo=1;//当前页，第一页从 0 开始，1 表示第二页
         int pageSize = 5;//每页显示多少条
         //设置查询分页
-        PageRequest pageRequest = PageRequest.of(currentPage, pageSize,sort);
+        PageRequest pageRequest = PageRequest.of(pageNo, pageSize,sort);
         //分页查询
         Page<Product> productPage = productDao.findAll(pageRequest);
         for (Product Product : productPage.getContent()) {
